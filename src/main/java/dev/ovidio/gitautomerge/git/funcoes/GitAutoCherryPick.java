c45bb243654882eb9ef8ec7f60521e5ebc60aa66
andre.ovidio <andre.ovidio@edusoft.com.br>
2022-11-22 19:09:04 -0300

    feat: adicionado log do link do commit quando feito via gitlab ci

--- src/main/java/dev/ovidio/gitautomerge/git/funcoes/GitAutoCherryPick.java
+++ src/main/java/dev/ovidio/gitautomerge/git/funcoes/GitAutoCherryPick.java
@@ -1,15 +1,15 @@
 package dev.ovidio.gitautomerge.git.funcoes;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import dev.ovidio.gitautomerge.exception.BaseException;
 import dev.ovidio.gitautomerge.exception.CherryPickException;
 import dev.ovidio.gitautomerge.git.integration.CherryPickResponse;
 import dev.ovidio.gitautomerge.git.model.Branch;
 import dev.ovidio.gitautomerge.git.model.ReleaseBranch;
 import dev.ovidio.gitautomerge.git.model.RepositorioGit;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 
 public class GitAutoCherryPick extends GitFuncaoBase{
 
@@ -35,22 +35,42 @@ public class GitAutoCherryPick extends GitFuncaoBase{
                         falhasCherryPick.add(b);
                     }
                 });
-        var sucessoBuilderStr = new StringBuilder();
-        sucessoBuilderStr.append("Branchs com sucesso: \n");
-        for (CherryPickResponse response : sucessoCherryPick){
-            sucessoBuilderStr.append(response.toString()+" \n");
-        }
-        System.out.println(sucessoBuilderStr);
+        gerLogSucesso(sucessoCherryPick);
 
-        var falhasStr = falhasCherryPick.stream()
-                .map(Branch::getFullBranchName)
-                .toList()
-                .toString();
-        System.err.println("Branchs com CONFLITO: "+falhasStr);
+        geraLogFalha(falhasCherryPick);
 
         if(!falhasCherryPick.isEmpty()){
             throw new BaseException("Houveram conflitos no cherry-pick automatico");
         }
     }
 
+	private void geraLogFalha(List<Branch> falhasCherryPick) {
+		var falhasStr = falhasCherryPick.stream()
+                .map(Branch::getFullBranchName)
+                .toList()
+                .toString();
+        System.err.println("Branchs com CONFLITO: "+falhasStr);
+	}
+
+	private void gerLogSucesso(List<CherryPickResponse> sucessoCherryPick) {
+		var sucessoBuilderStr = new StringBuilder();
+        sucessoBuilderStr.append("Branchs com sucesso: \n");
+        final var CI_PROJECT_URL = System.getenv("CI_PROJECT_URL");
+        for (CherryPickResponse response : sucessoCherryPick){
+            sucessoBuilderStr.append(response.toString()+" \n");
+            if(CI_PROJECT_URL != null && !CI_PROJECT_URL.isBlank()) {
+            	sucessoBuilderStr.append(
+            			CI_PROJECT_URL +"/-/commit/"+ response.getCommitId()+" \n"
+        			);            
+            }
+        }
+        final var NUMERO_CHAMADO = System.getenv("NUMERO_CHAMADO");
+        final var URL_SOFTDESK = System.getenv("URL_SOFTDESK");
+        if(NUMERO_CHAMADO != null && !NUMERO_CHAMADO.isBlank()
+        		&& URL_SOFTDESK != null && !URL_SOFTDESK.isBlank()) {
+        	sucessoBuilderStr.append(URL_SOFTDESK+NUMERO_CHAMADO+"\n");
+        }
+        System.out.println(sucessoBuilderStr);
+	}
+    
 }
